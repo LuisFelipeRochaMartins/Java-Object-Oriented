fa2e9285f3b8625b0db27feba80939dac097e0e9
Luís Felipe Rocha Martins <luisfelipetochamartins>
2022-10-09 21:34:43 -0300

    Commit

--- src/Exercise08.java
+++ src/Exercise08.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Exercise08 {
+import Objects.Bank;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise08 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random generator = new Random();
+
+        System.out.println("Creating account... ");
+        int n = generator.nextInt(1, 9999);
+
+        System.out.print("Type your name: ");
+        String name = input.nextLine();
+
+        System.out.print("How much many you want to deposit? ");
+        double balance = input.nextDouble();
+
+        Bank bank = new Bank(n, name, balance);
+        System.out.println(bank);
+
+        while (true) {
+
+            String opc = input.nextLine();
+            System.out.print("Want to deposit? Y/N ");
+            opc = input.nextLine();
+            if(opc.equalsIgnoreCase("Y")) {
+                System.out.print("How much you want to deposit? ");
+                double x = input.nextDouble();
+                bank.deposit(x);
+                opc = input.nextLine();
+            }
+            System.out.print("Want to withdraw? Y/N");
+            opc = input.nextLine();
+            if(opc.equalsIgnoreCase("Y")){
+                double x = 0;
+                System.out.print("How much you want to withdrw? ");
+                x = input.nextDouble();
+                bank.withdraw(x);
+                opc = input.nextLine();
+            }
+            System.out.print("Want to leave? Y/N");
+            opc = input.nextLine();
+
+
+            if(opc.equalsIgnoreCase("Y")){
+                break;
+            }
+        }
+        System.out.println(bank);
+    }
 }
--- src/Objects/Bank.java
+++ src/Objects/Bank.java
@@ -1,2 +1,33 @@
-package Objects;public class Bank {
+package Objects;
+
+public class Bank {
+
+    private int account;
+    private String name;
+    private double balance;
+
+    public Bank (int account, String name, double balance){
+        this.account = account;
+        this.name = name;
+        this.balance=balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void deposit(double n){
+        this.balance+=n;
+    }
+    public void withdraw(double n){
+        this.balance-=n;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("");
+        sb.append("Account : ").append(account+"\n");
+        sb.append("Name : ").append(name+"\n");
+        sb.append("Balance :").append(balance+"\n");
+        return sb.toString();
+    }
 }
